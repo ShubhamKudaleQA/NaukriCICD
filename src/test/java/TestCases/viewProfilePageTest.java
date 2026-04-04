b0cd078a9b48dbf262ca99d03caa4ee8ee6e87ba
Sai7214 <kshoo7214@gmail.com>
2026-04-04 20:01:46 +0530

    New Commit

--- src/test/java/TestCases/viewProfilePageTest.java
+++ src/test/java/TestCases/viewProfilePageTest.java
@@ -1,7 +1,5 @@
 package TestCases;
 
-import static org.testng.Assert.assertEquals;
-
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
