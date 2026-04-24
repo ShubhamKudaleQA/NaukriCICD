e034f4488fb524a3b39085540f7fc99488742ea0
Sai7214 <kshoo7214@gmail.com>
2026-04-24 13:17:20 +0530

    New Release 24/04

--- src/test/java/TestBase/TestBase.java
+++ src/test/java/TestBase/TestBase.java
@@ -1,19 +1,15 @@
 package TestBase;
 
-
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
-
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import com.google.common.io.Files;
 
 public class TestBase {
@@ -31,8 +27,9 @@ public class TestBase {
 		}
 
 	}
+
 	public void intilization() {
-		
+
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--start-maximized");
 		driver = new ChromeDriver(options);
@@ -42,12 +39,11 @@ public class TestBase {
 		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		driver.get(prop.getProperty("url"));
 	}
-	
-	
-	public String getScreenshot(String testName,WebDriver driver) throws Exception {
-				
-		File source = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);		
-		String path = System.getProperty("user.dir")+"//report//"+testName+".png";
+
+	public String getScreenshot(String testName, WebDriver driver) throws Exception {
+
+		File source = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String path = System.getProperty("user.dir") + "//report//" + testName + ".png";
 		File destination = new File(path);
 		Files.copy(source, destination);
 		return path;
--- src/test/java/TestCases/HomePageTest.java
+++ src/test/java/TestCases/HomePageTest.java
@@ -3,7 +3,6 @@ package TestCases;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import Pages.HomePage;
 import Pages.LoginPage;
 import Pages.ViewProfilePage;
